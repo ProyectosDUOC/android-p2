f7b5d6751320d6c0f45aba64869d2b0546f52229
Benjamin <benja.mora.torres@gmail.com>
2018-11-22 11:53:24 -0300

    2

--- Categoria.kt
+++
@@ -1,8 +0,0 @@
-package win.bemtorres.servicio.controldeinventario
-
-class Categoria (var idCategoria:Int, var nombreCategoria: String, var estado: Int=1){
-    override fun toString(): String {
-        return  nombreCategoria
-    }
-
-}
